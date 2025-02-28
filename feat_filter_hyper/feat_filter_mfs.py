5eef56b0fd7f29a2eb5b151b1bba5c122dcb937c
saif8091 <ms4667@rit.edu>
2025-02-28 15:19:26 -0800

    normalisation added code still too slow

--- feat_filter_hyper/feat_filter_mfs.py
+++ feat_filter_hyper/feat_filter_mfs.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 from src.feature_selection import FilterBasedFeatureSelection
+from src.model_utils import normalize_features
 
 print('Performing mutual information based feature selection')
 with pd.HDFStore('data/preprocessed_data/feat_h_split_comb.h5', mode='r') as store:
@@ -15,7 +16,10 @@ with pd.HDFStore('data/preprocessed_data/feat_h_split_comb.h5', mode='r') as sto
     labels_val = store['labels_val']
     labels_test = store['labels_test']
 
-mfs = FilterBasedFeatureSelection(pd.concat([X_train,X_val]),pd.concat([y_train,y_val]),handle='mi')
+X_train_norm,X_val_norm, norm_model = normalize_features(X_train,X_val)
+X_train_norm,X_test_norm,_ = normalize_features(X_train,X_test)
+
+mfs = FilterBasedFeatureSelection(pd.concat([X_train_norm,X_val_norm]),pd.concat([y_train,y_val]),handle='mi')
 selected_features = mfs.select_based_on_threshold(min_r2=0.50, r2_thresh=0.90)
 open('feat_filter_hyper/filtered_features/mfs.txt', 'w').writelines(f"{item}\n" for item in selected_features)
 print(f'Number of features selected for mfs: {len(selected_features)}')
--- feat_filter_hyper/feat_filter_micorfs.py
+++ feat_filter_hyper/feat_filter_micorfs.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 from src.feature_selection import FilterBasedFeatureSelection
+from src.model_utils import normalize_features
 
 print('Performing correlation and mutual information based feature selection')
 with pd.HDFStore('data/preprocessed_data/feat_h_split_comb.h5', mode='r') as store:
@@ -15,7 +16,10 @@ with pd.HDFStore('data/preprocessed_data/feat_h_split_comb.h5', mode='r') as sto
     labels_val = store['labels_val']
     labels_test = store['labels_test']
 
-micorfs = FilterBasedFeatureSelection(pd.concat([X_train,X_val]),pd.concat([y_train,y_val]),handle='mi_cor')
+X_train_norm,X_val_norm, norm_model = normalize_features(X_train,X_val)
+X_train_norm,X_test_norm,_ = normalize_features(X_train,X_test)
+
+micorfs = FilterBasedFeatureSelection(pd.concat([X_train_norm,X_val_norm]),pd.concat([y_train,y_val]),handle='mi_cor')
 selected_features = micorfs.select_based_on_threshold(min_r2=0.75, r2_thresh=0.80)
 open('feat_filter_hyper/filtered_features/micorfs.txt', 'w').writelines(f"{item}\n" for item in selected_features)
 print(f'Number of features selected for micorfs: {len(selected_features)}')
